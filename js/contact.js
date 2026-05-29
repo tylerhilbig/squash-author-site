9b8028656d2aa99ac95ff389049c32c94e025a69
Tyler Hilbig <bighilltyler@gmail.com>
2026-05-29 10:17:38 -0600

    Added the live endpoint for formspree.

--- js/contact.js
+++ js/contact.js
@@ -6,37 +6,36 @@ export function renderContact(data) {
 
 const form = document.getElementById("contact-form");
 
-// form.addEventListener("submit", async (event) => {
-//     event.preventDefault();
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-//     const formData = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         message: document.getElementById("message").value
-//     };
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
 
-//     // TODO Temporary endpoint for testing form submission - replace with actual endpoint when ready
-//     try {
-//         const response = await fetch("https://formspree.io/f/abcxyz", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(formData)
-//         });
+    try {
+        const response = await fetch("https://formspree.io/f/xykvvedw", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
 
-//         if (response.ok) {
-//             alert("Thank you for your message!");
-//             form.reset();
-//         } else {
-//             alert("There was an error submitting the form. Please try again later.");
-//         }
+        if (response.ok) {
+            alert("Thank you for your message!");
+            form.reset();
+        } else {
+            alert("There was an error submitting the form. Please try again later.");
+        }
 
-//     } catch (error) {
-//         console.error("Error submitting form:", error);
-//     }
+    } catch (error) {
+        console.error("Error submitting form:", error);
+    }
 
-//     console.log("Form data:", { name, email, message });
+    console.log("Form data:", { name, email, message });
 
-//     console.log("Form submitted!");
-// })
+    console.log("Form submitted!");
+})
